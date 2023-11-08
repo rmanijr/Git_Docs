1994cbc45f89a0a45ab532476024059c289a45aa
Manikandan <rmanijr@yahoo.com>
2023-11-08 14:40:55 +0530

    updated text test3 in run,bat

--- run.bat
+++ run.bat
@@ -2,4 +2,5 @@ systeminfo   | find "System Boot Time:"  1>c:\temp\jen_out.txt
 echo "Date:" %Date%  1>>c:\temp\jen_out.txt 
 echo "Time:" %Time%  1>>c:\temp\jen_out.txt 
 echo "test1"
+echo "test3"
 echo "test2"
