6152292cd665ad9d7e569ed430cb954764cccecb
Manikandan <rmanijr@yahoo.com>
2023-11-08 14:36:22 +0530

    run.bat updated

--- run.bat
+++ run.bat
@@ -1,3 +1,5 @@
 systeminfo   | find "System Boot Time:"  1>c:\temp\jen_out.txt
 echo "Date:" %Date%  1>>c:\temp\jen_out.txt 
 echo "Time:" %Time%  1>>c:\temp\jen_out.txt 
+echo "test1"
+echo "test2"
